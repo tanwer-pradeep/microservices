db018c39775b70ed70355918a1cdce4f950c7d98
Pradeep Singh <pradeep.s@opensenselabs.com>
2022-09-03 18:35:05 +0530

    Updated packages and installed mongoose

--- posts/index.js
+++ posts/index.js
@@ -3,21 +3,36 @@ const { randomBytes } = require("crypto");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const axios = require("axios");
+// const p = require("./connect");
+// const posts = require('./posts')
+// import main from "./connect";
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
-
-const posts = {};
-
+const postss = {};
+// p.main()
 app.post("/posts", async (req, res) => {
   const postID = randomBytes(4).toString("hex");
   const { title } = req.body;
 
-  posts[postID] = {
+  postss[postID] = {
     postID,
     title,
   };
+  // try {
+  //   posts.posts.create({
+  //     postID:{
+  //       postID,
+  //       title// String is shorthand for {type: String}
+  
+  //   }
+  //   })
+    
+  // } catch (error) {
+  //   console.log("error post")
+    
+  // }
 
   // requesting to events services
   await axios.post("http://localhost:4001/events", {
@@ -27,7 +42,7 @@ app.post("/posts", async (req, res) => {
       title,
     },
   });
-  res.status(201).send(posts[postID]);
+  res.status(201).send(postss[postID]);
 });
 
 app.post("/events", (req, res) => {
@@ -36,7 +51,7 @@ app.post("/events", (req, res) => {
 });
 
 app.get("/posts", (req, res) => {
-  res.send(posts);
+  res.send(postss);
 });
 
 app.listen(3001, () => {
